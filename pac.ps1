30f58f6ac9d22f119ac16f3fcaa12e9f38797ce8
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-05-23 23:36:47 +0300

    Create new release 0.1.0

--- pac.ps1
+++ pac.ps1
@@ -1,6 +1,6 @@
 dotnet publish -c Release  ..\LibraLoan\ -r win-x64 -o .\publish
 
-$version = "0.0.15"
+$version = "0.1.0"
 $pack_id = "LibraLoan"
 $main_exe = "LibraLoan.exe"
 $icon_path = ".\icon.ico"
